a73185466f968332ca9b004f597412f438f1b3b5
Messias Martins <messiasmartins@outlook.com>
2026-04-01 14:41:10 -0300

    Soften FAQ visuals with warm tones and smoother accordion transitions

--- data/headerNavLinks.js
+++ data/headerNavLinks.js
@@ -1,10 +1,9 @@
 const headerNavLinks = [
   { href: '/', title: 'Inicio' },
+  { href: '/blog', title: 'Postagem' },
   { href: '/#faq', title: 'FAQ' },
-  //{ href: '/products', title: 'Produtos' },
-  { href: '/blog', title: 'Postagens' },
-  { href: '/tags', title: 'Tags' },
   { href: '/about', title: 'Sobre' },
+  { href: '/tags', title: 'Tags' },
 ]
 
 export default headerNavLinks
