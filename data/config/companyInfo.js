47fb705d089aa7e0164a5eeb4ed9663164fc7792
Messias Martins <messiasmartins@outlook.com>
2026-05-01 07:56:57 -0300

    Corrige numero do WhatsApp

--- data/config/companyInfo.js
+++ data/config/companyInfo.js
@@ -31,7 +31,7 @@ const companyInfo = {
   ],
   links: {
     whatsapp:
-      'https://api.whatsapp.com/send?phone=5531992482390&text=Ol%C3%A1!%20Vim%20pelo%20link%20do%20WhatsApp%20no%20site.%20Quero%20saber%20mais%20sobre%20seus%20produtos%20e%20servi%C3%A7os.',
+      'https://api.whatsapp.com/send?phone=553136532390&text=Ol%C3%A1!%20Vim%20pelo%20link%20do%20WhatsApp%20no%20site.%20Quero%20saber%20mais%20sobre%20seus%20produtos%20e%20servi%C3%A7os.',
     instagram: 'https://www.instagram.com/madeirassantos/',
     googleBusiness:
       'https://www.google.com/maps/place/Madeiras+Santos/@-19.8436181,-43.9877025,15z/data=!4m6!3m5!1s0x0:0xeae93a7279947fed!8m2!3d-19.8436181!4d-43.9877025!16s%2Fg%2F11j1nzd0tr',
